08dcbd761eb99fc9aeac879296bc16c138cb7f8b
soyaoki <-@gmail.com>
2024-03-24 18:00:06 +0900

    update: updated files for submission.

--- main.py
+++ main.py
@@ -8,25 +8,10 @@ import os
 from ml.data import process_data
 from ml.model import inference
 
+from pydantic_model import InputData
+
 # Instantiate the app.
 app = FastAPI()
-
-
-class InputData(BaseModel):
-    age: int = Field(..., example=30)
-    workclass: str = Field(..., example="State-gov")
-    fnlgt: int = Field(..., example=77516)
-    education: str = Field(..., example="Bachelors")
-    education_num: int = Field(..., example=13)
-    marital_status: str = Field(..., example="Never-married")
-    occupation: str = Field(..., example="Adm-clerical")
-    relationship: str = Field(..., example="Not-in-family")
-    race: str = Field(..., example="White")
-    sex: str = Field(..., example="Male")
-    capital_gain: int = Field(..., example=2174)
-    capital_loss: int = Field(..., example=0)
-    hours_per_week: int = Field(..., example=40)
-    native_country: str = Field(..., example="United-States")
 
 
 # Define a GET on the specified endpoint.
---
+++ pydantic_model.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field
+
+class InputData(BaseModel):
+    age: int = Field(..., example=30)
+    workclass: str = Field(..., example="State-gov")
+    fnlgt: int = Field(..., example=77516)
+    education: str = Field(..., example="Bachelors")
+    education_num: int = Field(..., example=13)
+    marital_status: str = Field(..., example="Never-married")
+    occupation: str = Field(..., example="Adm-clerical")
+    relationship: str = Field(..., example="Not-in-family")
+    race: str = Field(..., example="White")
+    sex: str = Field(..., example="Male")
+    capital_gain: int = Field(..., example=2174)
+    capital_loss: int = Field(..., example=0)
+    hours_per_week: int = Field(..., example=40)
+    native_country: str = Field(..., example="United-States")
+    
